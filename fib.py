2ef50e2d8fd5fb82188931d6aacdd17c6227ef09
smclarnon <smclarnon@gmail.com>
2018-11-14 13:09:19 -0500

    hah

--- fib.py
+++ fib.py
@@ -2,26 +2,32 @@ from unittest import TestCase
 
 
 # classic recursive solution
-def fib_classic(n, _):
+def fib_classic(n):
     if n in [1, 2]:
         return 1
     return fib_classic(n - 1) + fib_classic(n - 2)
 
 
 # recursive with memoization
-def fib_recursive_with_memo(n, memo):
-    if memo[n]:
-        return memo[n]
-    if n in [1, 2]:
-        result = 1
-    else:
-        result = fib_recursive_with_memo(n - 1, memo) + fib_recursive_with_memo(n - 2, memo)
-    memo[n] = result
-    return result
+def fib_recursive_with_memo(n):
+    memo = [None] * (n + 1)
+
+    def _fib(x):
+        if memo[x]:
+            return memo[x]
+        if x in [1, 2]:
+            result = 1
+        else:
+            result = _fib(x - 1) + _fib(x - 2)
+        memo[x] = result
+        return result
+
+    return _fib(n)
 
 
 # bottom up approach
-def fib_bottom_up(n, memo):
+def fib_bottom_up(n):
+    memo = [None] * (n + 1)
     for i in range(1, n + 1):
         if i in [1, 2]:
             memo[i] = 1
@@ -31,15 +37,23 @@ def fib_bottom_up(n, memo):
     return memo[n]
 
 
-def fib(n):
-    memo = [None] * (n + 1)
-    return fib_bottom_up(n, memo)
-
-
 class FibTest(TestCase):
 
+    def test_classic(self):
+        self.assertEqual(2, fib_classic(3))
+        self.assertEqual(5, fib_classic(5))
+        self.assertEqual(8, fib_classic(6))
+        # too slow
+        # self.assertEqual(280571172992510140037611932413038677189525, fib_classic(200))
+
+    def test_memo(self):
+        self.assertEqual(2, fib_recursive_with_memo(3))
+        self.assertEqual(5, fib_recursive_with_memo(5))
+        self.assertEqual(8, fib_recursive_with_memo(6))
+        self.assertEqual(280571172992510140037611932413038677189525, fib_recursive_with_memo(200))
+
     def test_should_do_something(self):
-        # self.assertEqual(2, fib(3))
-        # self.assertEqual(5, fib(5))
-        # self.assertEqual(8, fib(6))
-        self.assertEqual(280571172992510140037611932413038677189525, fib(200))
+        self.assertEqual(2, fib_bottom_up(3))
+        self.assertEqual(5, fib_bottom_up(5))
+        self.assertEqual(8, fib_bottom_up(6))
+        self.assertEqual(280571172992510140037611932413038677189525, fib_bottom_up(200))
